3d628e8ffed6c5dc981144508fbbee33885b090e
Matthew D. Steele <mdsteele@alum.mit.edu>
2018-02-28 09:44:47 -0500

    Support encoding/decoding PNG icons

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,19 @@ extern crate byteorder;
 extern crate png;
 
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use png::HasParameters;
+use std::{u16, u8};
 use std::io::{self, Read, Seek, SeekFrom, Write};
-use std::u16;
+
+// ========================================================================= //
+
+// The signature that all PNG files start with.
+const PNG_SIGNATURE: &[u8] = &[0x89, b'P', b'N', b'G'];
+
+const MIN_WIDTH: u32 = 1;
+const MAX_WIDTH: u32 = 256;
+const MIN_HEIGHT: u32 = 1;
+const MAX_HEIGHT: u32 = 256;
 
 // ========================================================================= //
 
@@ -84,6 +95,23 @@ impl IconDir {
 
     /// Returns the entries in this collection.
     pub fn entries(&self) -> &[IconDirEntry] { &self.entries }
+
+    /// Encodes an image as a new entry in this collection.  Returns an error
+    /// if the encoding fails.
+    pub fn add_entry(&mut self, image: IconImage) -> io::Result<()> {
+        let mut data = Vec::new();
+        image.to_png(&mut data)?;
+        let entry = IconDirEntry {
+            width: image.width(),
+            height: image.height(),
+            num_colors: 0,
+            color_planes: 0,
+            bits_per_pixel: 32,
+            data,
+        };
+        self.entries.push(entry);
+        Ok(())
+    }
 
     /// Reads an ICO file into memory.
     pub fn read<R: Read + Seek>(mut reader: R) -> io::Result<IconDir> {
@@ -178,7 +206,7 @@ impl IconDir {
 
 // ========================================================================= //
 
-/// One entry in an ICO file; a single image or cursor.
+/// One entry in an ICO file; a single icon or cursor.
 pub struct IconDirEntry {
     width: u32,
     height: u32,
@@ -194,13 +222,179 @@ impl IconDirEntry {
 
     /// Returns the height of the image, in pixels.
     pub fn height(&self) -> u32 { self.height }
+
+    /// Decodes this entry into an image.  Returns an error if the data is
+    /// malformed or can't be decoded.
+    pub fn decode(&self) -> io::Result<IconImage> {
+        if self.data.starts_with(PNG_SIGNATURE) {
+            let image = IconImage::from_png(self.data.as_slice())?;
+            if image.width != self.width || image.height != self.height {
+                invalid_data!("Encoded PNG has wrong dimensions \
+                               (was {}x{}, but should be {}x{})",
+                              image.width,
+                              image.height,
+                              self.width,
+                              self.height);
+            }
+            Ok(image)
+        } else {
+            // TODO: Implement BMP decoding
+            invalid_data!("Decoding non-PNG images is not yet supported");
+        }
+    }
+}
+
+// ========================================================================= //
+
+/// A decoded image from an ICO file.
+pub struct IconImage {
+    width: u32,
+    height: u32,
+    rgba_data: Vec<u8>,
+}
+
+impl IconImage {
+    /// Creates a new image with the given dimensions and RGBA data.  The
+    /// `width` and `height` must each be between 1 and 256 inclusive, and
+    /// `rgba_data` must have `4 * width * height` bytes and be in row-major
+    /// order.  Panics if the dimensions are out of range or if `rgba_data` is
+    /// the wrong length.
+    pub fn from_rgba_data(width: u32, height: u32, rgba_data: Vec<u8>)
+                          -> IconImage {
+        if width < MIN_WIDTH || width > MAX_WIDTH {
+            panic!("Invalid width (was {}, but range is {}-{})",
+                   width,
+                   MIN_WIDTH,
+                   MAX_WIDTH);
+        }
+        if height < MIN_HEIGHT || height > MAX_HEIGHT {
+            panic!("Invalid height (was {}, but range is {}-{})",
+                   height,
+                   MIN_HEIGHT,
+                   MAX_HEIGHT);
+        }
+        let expected_data_len = (4 * width * height) as usize;
+        if rgba_data.len() != expected_data_len {
+            panic!("Invalid data length \
+                    (was {}, but must be {} for {}x{} image)",
+                   rgba_data.len(),
+                   expected_data_len,
+                   width,
+                   height);
+        }
+        IconImage {
+            width,
+            height,
+            rgba_data,
+        }
+    }
+
+    /// Decodes an image from a PNG file.  The width and height of the image
+    /// must each be between 1 and 256 inclusive.  Returns an error if the PNG
+    /// data is malformed or can't be decoded, or if the size of the PNG image
+    /// is out of range.
+    pub fn from_png<R: Read>(reader: R) -> io::Result<IconImage> {
+        let decoder = png::Decoder::new(reader);
+        let (info, mut reader) = match decoder.read_info() {
+            Ok(tuple) => tuple,
+            Err(error) => invalid_data!("Malformed PNG data; {}", error),
+        };
+        if info.width < MIN_WIDTH || info.width > MAX_WIDTH {
+            invalid_data!("Invalid width (was {}, but range is {}-{})",
+                          info.width,
+                          MIN_WIDTH,
+                          MAX_WIDTH);
+        }
+        if info.height < MIN_HEIGHT || info.height > MAX_HEIGHT {
+            invalid_data!("Invalid height (was {}, but range is {}-{})",
+                          info.height,
+                          MIN_HEIGHT,
+                          MAX_HEIGHT);
+        }
+        if info.bit_depth != png::BitDepth::Eight {
+            // TODO: Support other bit depths.
+            invalid_data!("Unsupported PNG bit depth: {:?}", info.bit_depth);
+        }
+        let mut buffer = vec![0u8; info.buffer_size()];
+        match reader.next_frame(&mut buffer) {
+            Ok(()) => {}
+            Err(error) => invalid_data!("Malformed PNG data; {}", error),
+        }
+        let rgba_data = match info.color_type {
+            png::ColorType::RGBA => buffer,
+            png::ColorType::RGB => {
+                let num_pixels = buffer.len() / 3;
+                let mut rgba = Vec::with_capacity(num_pixels * 4);
+                for i in 0..num_pixels {
+                    rgba.extend_from_slice(&buffer[(3 * i)..][..3]);
+                    rgba.push(u8::MAX);
+                }
+                rgba
+            }
+            png::ColorType::GrayscaleAlpha => {
+                let num_pixels = buffer.len() / 2;
+                let mut rgba = Vec::with_capacity(num_pixels * 4);
+                for i in 0..num_pixels {
+                    let gray = buffer[2 * i];
+                    let alpha = buffer[2 * i + 1];
+                    rgba.push(gray);
+                    rgba.push(gray);
+                    rgba.push(gray);
+                    rgba.push(alpha);
+                }
+                rgba
+            }
+            png::ColorType::Grayscale => {
+                let mut rgba = Vec::with_capacity(buffer.len() * 4);
+                for value in buffer.into_iter() {
+                    rgba.push(value);
+                    rgba.push(value);
+                    rgba.push(value);
+                    rgba.push(std::u8::MAX);
+                }
+                rgba
+            }
+            png::ColorType::Indexed => {
+                // TODO: Implement ColorType::Indexed conversion
+                invalid_data!("Unsupported PNG color type: {:?}",
+                              info.color_type);
+            }
+        };
+        Ok(IconImage::from_rgba_data(info.width, info.height, rgba_data))
+    }
+
+    /// Encodes the image as a PNG file.
+    pub fn to_png<W: Write>(&self, writer: W) -> io::Result<()> {
+        let mut encoder = png::Encoder::new(writer, self.width, self.height);
+        encoder.set(png::ColorType::RGBA).set(png::BitDepth::Eight);
+        let result =
+            encoder.write_header().and_then(|mut writer| {
+                writer.write_image_data(&self.rgba_data)
+            });
+        match result {
+            Ok(()) => Ok(()),
+            Err(png::EncodingError::IoError(error)) => return Err(error),
+            Err(png::EncodingError::Format(error)) => {
+                invalid_input!("PNG format error: {}", error);
+            }
+        }
+    }
+
+    /// Returns the width of the image, in pixels.
+    pub fn width(&self) -> u32 { self.width }
+
+    /// Returns the height of the image, in pixels.
+    pub fn height(&self) -> u32 { self.height }
+
+    /// Returns the RGBA data for this image, in row-major order.
+    pub fn rgba_data(&self) -> &[u8] { &self.rgba_data }
 }
 
 // ========================================================================= //
 
 #[cfg(test)]
 mod tests {
-    use super::{IconDir, ResourceType};
+    use super::{IconDir, IconImage, ResourceType};
     use std::io::Cursor;
 
     #[test]
@@ -244,6 +438,33 @@ mod tests {
         icondir.write(&mut output).unwrap();
         let expected: &[u8] = b"\x00\x00\x02\x00\x00\x00";
         assert_eq!(output.as_slice(), expected);
+    }
+
+    #[test]
+    fn image_data_round_trip() {
+        // Create an image:
+        let width = 11;
+        let height = 13;
+        let mut rgba = Vec::new();
+        for index in 0..(width * height) {
+            rgba.push(if index % 2 == 0 { 0 } else { 255 });
+            rgba.push(if index % 3 == 0 { 0 } else { 255 });
+            rgba.push(if index % 5 == 0 { 0 } else { 255 });
+            rgba.push(if index % 7 == 0 { 128 } else { 255 });
+        }
+        let image = IconImage::from_rgba_data(width, height, rgba.clone());
+        // Write that image into an ICO file:
+        let mut icondir = IconDir::new(ResourceType::Icon);
+        icondir.add_entry(image).unwrap();
+        let mut file = Vec::<u8>::new();
+        icondir.write(&mut file).unwrap();
+        // Read the ICO file back in and make sure the image is the same:
+        let icondir = IconDir::read(Cursor::new(&file)).unwrap();
+        assert_eq!(icondir.entries().len(), 1);
+        let image = icondir.entries()[0].decode().unwrap();
+        assert_eq!(image.width(), width);
+        assert_eq!(image.height(), height);
+        assert_eq!(image.rgba_data(), rgba.as_slice());
     }
 }
 
